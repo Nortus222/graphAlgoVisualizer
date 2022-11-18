12b54727c8147080c77c3318a07e5599df8fa805
IhorSherstiuk <mycat201198@gmail.com>
2022-11-18 12:10:44 -0800

    Add styling and UI redisign

--- graphCanvas.py
+++ graphCanvas.py
@@ -8,14 +8,18 @@ from graph import Graph
 
 
 class GraphCanvas(FigureCanvas):
-    def __init__(self, parent=None):
+    def __init__(self, interval=None):
         figure = plt.figure()
         super(GraphCanvas, self).__init__(figure)
+        self.interval = interval
+  
 
-    def loadGraph(self, datasetName):
+    def loadGraph(self, datasetName) -> Graph:
       self.g = Graph(datasetName)
       
       self.drawIncomplete()
+
+      return self.g
       
       
 
@@ -26,45 +30,58 @@ class GraphCanvas(FigureCanvas):
 
     def drawIncomplete(self):
       self.figure.clf()
-      nx.draw_networkx(self.g, pos=self.g.pos, edgelist=self.g.edgesListIncomplete, with_labels=True)
+      nx.draw(self.g, pos=self.g.pos, edgelist=self.g.edgesListIncomplete, with_labels=True)
       self.draw()
 
     def drawEmpty(self):
       self.figure.clf()
-      nx.draw_networkx(self.g, pos=self.g.pos, edgelist=[], with_labels=True)
+      nx.draw(self.g, pos=self.g.pos, edgelist=[], with_labels=True)
       self.draw()
 
     def reset(self, algo):
-        if (algo == "Kruskal's" or algo == "Prim's"):
-            self.drawIncomplete()
-
-    def mst(self, algo):
-        if (algo == "Kruskal's"):
-            mst = self.g.kruskals()
-        elif (algo == "Prim's"):
-            mst = self.g.prims()
-        self.drawMST(mst=mst)
-
-    def drawMST(self, mst):
-      self.figure.clf()
-      nx.draw_networkx(self.g, pos=self.g.pos, edgelist=mst, with_labels=True)
-      self.draw()
+      self.drawIncomplete()
 
     def animEmpty(self):
       self.figure.clf()
-      nx.draw_networkx(self.g, pos=self.g.pos, edgelist=[], node_size=100, with_labels=False)
+      nx.draw(self.g, pos=self.g.pos, edgelist=[], node_size=100, with_labels=False)
 
-    def animate(self, frame, mst):
+    def animIncomplete(self):
+      self.figure.clf()
+      nx.draw(self.g, pos=self.g.pos, edgelist=self.g.edgesListIncomplete, with_labels=True)
+
+    def animateMST(self, frame, mst):
       nx.draw_networkx_edges(self.g, pos=self.g.pos, edgelist=mst[0:frame])
 
-    def visualize(self, algo):
-      if (algo == "Kruskal's"):
-            mst = self.g.kruskals()
-      elif (algo == "Prim's"):
-            mst = self.g.prims()
+    def animateShortestPath(self, frame, fullPath, path):
+      if (len(fullPath)+1 < frame):
+        self.drawIncomplete()
+        nx.draw_networkx_edges(self.g, pos=self.g.pos, edgelist=path, edge_color=(1,0,0), width=2)
+      else:
+        nx.draw_networkx_edges(self.g, pos=self.g.pos, edgelist=fullPath[0:frame], edge_color=(1,0,0), width=2)
 
-      _ = animation.FuncAnimation(self.figure, partial(self.animate, mst=mst), frames=len(mst)+1, interval=400, repeat=False, init_func=self.animEmpty)
+    def visualize(self, algo, startNode=None, endNode=None):
+      print(startNode, endNode)
+      if (algo == "Kruskal's"):
+            self.visualizeMST(self.g.kruskals())
+      elif (algo == "Prim's"):
+            self.visualizeMST(self.g.prims())
+      elif (algo == "Dijkstra's"):
+        path, fullpath = self.g.dijkstra(startNode, endNode)
+        self.visualizeShortestPath(path, fullpath)
+      elif (algo == "Bellman Ford's"):
+        path, fullpath = self.g.bellmanFord(startNode, endNode)
+        self.visualizeShortestPath(path, fullpath)
+
+    def visualizeMST(self, mst):
+      _ = animation.FuncAnimation(self.figure, partial(self.animateMST, mst=mst), frames=len(mst)+1, interval=self.interval.value(), repeat=False, init_func=self.animEmpty)
       self.draw()
+
+    def visualizeShortestPath(self, path, fullPath):
+      _ = animation.FuncAnimation(self.figure, partial(self.animateShortestPath, fullPath=fullPath, path=path), frames=len(fullPath)+3, interval=self.interval.value(), repeat=False, init_func=self.animIncomplete)
+      self.draw()
+      
+
+      
 
       
 
---
+++ qStyle.py
@@ -0,0 +1,43 @@
+style = """
+
+    QWidget#centralwidget{
+    background-color: #F5EEE8;      
+    }
+
+    QPushButton {
+    background-color: #f1f2f6;
+    border: 1px solid #0984e3;
+    color: #0984e3;
+    border-radius: 4px;
+    padding: 3px;
+    min-width: 60px;
+    }
+
+    QPushButton:hover {
+    border: 1px solid #0984e3;
+    background-color: #0984e3;
+    color: white;
+    }
+
+    QPushButton:pressed {
+    background-color: #74b9ff;
+    border: 1px solid #74b9ff;
+    }
+
+    QPushButton:focus {
+    border: 1px solid #0984e3;
+    }
+
+    QGroupBox{
+      border: 1px solid #0984e3;
+      background-color: #f1f2f6;
+      border-radius: 4px;
+    }
+
+    QFrame#frame{
+      border: 1px solid #0984e3;
+      border-radius: 4px;
+    }
+    
+  
+                """
